8a9c9f32a0a7b2770fa426cee982594da69b88e1
Subhadeep-Dhar <subhadeepdhar563@gmail.com>
2026-05-22 20:06:01 +0530

    structural fix

---
+++ poultry-disease-app/src/navigation/AppNavigator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// TODO: Import navigation stack and screens here
+
+export const AppNavigator: React.FC = () => {
+  // TODO: Implement navigation structure
+  return null;
+};
+
+export default AppNavigator;
