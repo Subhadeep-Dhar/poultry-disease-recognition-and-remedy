dfeb94cb783b4fb269b5ea4791bbe1946d8f5ed1
Subhadeep-Dhar <subhadeepdhar563@gmail.com>
2026-05-23 16:00:04 +0530

    Initial phase 4 completed

---
+++ poultry-disease-app/webpack.config.js
@@ -0,0 +1,18 @@
+const createExpoWebpackConfig = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfig(env, argv);
+
+  config.resolve = config.resolve || {};
+  config.resolve.alias = {
+    ...(config.resolve.alias || {}),
+    'react-native$': 'react-native-web',
+    'react-native-web/dist/exports/StyleSheet': require.resolve('react-native-web/dist/exports/StyleSheet'),
+    'react-native-web/dist/exports/StyleSheet/dom': require.resolve('react-native-web/dist/exports/StyleSheet/dom'),
+    'react-native-web/dist/exports/NativeEventEmitter': require.resolve('react-native-web/dist/exports/NativeEventEmitter'),
+    'react-native-web/dist/exports/Platform': require.resolve('react-native-web/dist/exports/Platform'),
+    'react-dom/client': require.resolve('react-dom/client'),
+  };
+
+  return config;
+};
